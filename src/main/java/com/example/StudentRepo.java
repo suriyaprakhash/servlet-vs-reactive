4f5abe7b3c3deaf5286da7c6ae62cf14bd39c8d8
suriyaprakhash <suriyaprakhash@gmail.com>
2025-02-16 17:05:39 -0500

    updated servlet vs reactive

--- src/main/java/com/example/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-//@RepositoryRestResource
-public interface StudentRepo extends JpaRepository<Student, Long>{
-//public interface StudentRepo extends CrudRepository<Student, String>{
-}
---
+++ src/main/java/com/suriyaprakhash/learn/reactive_web/hugefile/data/Pair.java
@@ -0,0 +1,6 @@
+package com.suriyaprakhash.learn.reactive_web.hugefile.data;
+
+
+public record Pair<K, V>(K key, V value) {
+
+}
